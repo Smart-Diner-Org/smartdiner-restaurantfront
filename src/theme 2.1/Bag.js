2272d274c1deb4b63e924774d01301bf4ca2eb70
vinayveerappaji <vinayveerappaji@gmail.com>
2020-08-08 17:34:37 +0530

    changes made... success

--- src/theme 2.1/Bag.js
+++ src/theme 2.1/Bag.js
@@ -1,7 +1,6 @@
 import React , {Component} from 'react'
 import BagItemList from './BagItemList';
 import GetLocation from './GetLocation'
-import { geolocated } from "react-geolocated";
 import { getDistance } from 'geolib';
 import Geocode from "react-geocode";
 
@@ -9,6 +8,79 @@ import Geocode from "react-geocode";
 import Logo from './assets/images/bag.png'
 
 class Bag extends Component{  
+    constructor(props){
+        super(props);
+        this.state={
+            address:null,
+            lat:null,
+            long:null,
+            boundary:false,
+        }
+        this.checkDistance =  this.checkDistance.bind(this)
+        this.getCoords = this.getCoords.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+    }
+  
+    checkDistance(){
+        let distance = null;
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                    distance = ( getDistance({
+                        latitude: position.coords.latitude,
+                    longitude:position.coords.longitude},
+                     {
+                        latitude: 13.093410,
+                        longitude: 77.399053
+                    },1)
+                );
+                },
+
+            () => {
+                alert('Position could not be determined.');
+            }
+        );
+        if(distance>=10000){
+            alert("Sorry for our Incovenience.... You're out of our boundary")
+        }else{
+            alert("Welcome you sir... we are happy to serve you")
+            this.setState({boundary:true})
+        }
+        
+    }
+
+    handleChange(event) {
+        this.setState({address: event.target.value});
+      }
+
+    getCoords(event){
+        console.log(`${this.state.address}`)
+        Geocode.fromAddress(`${this.state.address}`,null,null,'bangalore').then(
+            response => {
+            const { lat, lng } = response.results[0].geometry.location;
+            
+            let distance = ( getDistance({
+                latitude:lat,
+                longitude: lng},
+             {
+                latitude: 12.988061,
+                longitude: 77.576988
+            },1))
+
+            console.log(distance)
+
+            if(distance>=10000){
+                alert("Sorry for our Incovenience.... You're out of our boundary")
+            }else{
+                alert("Welcome you sir... we are happy to serve you")
+                this.setState({boundary:true})
+            }
+            },
+            error => {
+              console.error(error);
+            }
+          );
+        event.preventDefault()
+    }
 
 
 
@@ -32,26 +104,29 @@ render(){
                         <label >Your PaperBag</label>
                         <img src={Logo} alt={Logo}/>
                         </div>
-
                     </div>
-                    
                 </header>
-                <GetLocation />
-                
+
+                {!this.state.boundary ?
+                <GetLocation getCoords={this.getCoords} handleChange={this.handleChange} checkDistance={this.checkDistance}/>
+                :
+                <>
                 <BagItemList 
                             items={this.props.items}
                             changequantity={this.props.changequantity}
                             
                         />
 
-                
-                {/* <footer>
+                <footer>
                     <div className='foot-content'>
                         <span className="span-left">Powered by <a href="">Smart Diner</a></span>
                         <span className="span-right"><a href='#'>Terms and Conditions</a>
                         <a href='#'>Contact Us</a></span>
                     </div>
-                </footer> */}
+                </footer>
+                </>
+
+                }
             </div>
             
         );
--- src/theme 2.1/GetLocation.js
+++ src/theme 2.1/GetLocation.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react"
 import { geolocated } from "react-geolocated";
-import { getDistance } from 'geolib';
 import Geocode from "react-geocode";
 // import LocationSearchInput from "./LocationSearchInput"
 import './map.css'
@@ -10,83 +9,9 @@ Geocode.enableDebug();
 
 
 class GetLocation extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            address:null,
-            lat:null,
-            long:null,
-        }
-        this.checkDistance =  this.checkDistance.bind(this)
-        this.getCoords = this.getCoords.bind(this)
-        this.handleChange = this.handleChange.bind(this);
-    }
-  
-    checkDistance(){
-        let distance = null;
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                    distance = ( getDistance({
-                        latitude: position.coords.latitude,
-                    longitude:position.coords.longitude},
-                     {
-                        latitude: 13.093410,
-                        longitude: 77.399053
-                    },1)
-                );
-                },
-
-            () => {
-                alert('Position could not be determined.');
-            }
-        );
-        if(distance>=10000){
-            alert("Sorry for our Incovenience.... You're out of our boundary")
-        }else{
-            alert("Welcome you sir... we are happy to serve you")
-        }
-        
-    }
-
-    handleChange(event) {
-        this.setState({address: event.target.value});
-      }
-
-    getCoords(event){
-        console.log(`${this.state.address}`)
-        Geocode.fromAddress(`${this.state.address}`,null,null,'bangalore').then(
-            response => {
-            const { lat, lng } = response.results[0].geometry.location;
-            
-            let distance = ( getDistance({
-                latitude:lat,
-                longitude: lng},
-             {
-                latitude: 12.988061,
-                longitude: 77.576988
-            },1))
-
-            console.log(distance)
-
-            if(distance>=10000){
-                alert("Sorry for our Incovenience.... You're out of our boundary")
-            }else{
-                alert("Welcome you sir... we are happy to serve you")
-            }
-
-
-            },
-            error => {
-              console.error(error);
-            }
-          );
-        event.preventDefault()
-    }
-
+    
     // lat: 13.093410, lng: 77.399053
 
-
-    
 
     render(){
         
@@ -96,10 +21,10 @@ class GetLocation extends Component{
             <div>Geolocation is not enabled</div>
         ) : this.props.coords ?(
             <div className="ask-location">
-                <form onSubmit={this.getCoords}>
-                <input type="text" placeholder="Enter a Location" onChange={this.handleChange}/>
+                <form onSubmit={this.props.getCoords}>
+                <input type="text" placeholder="Enter a Location" onChange={this.props.handleChange}/>
                 </form>
-                <button onClick={this.checkDistance}><i class="lni lni-map-marker mr-20"></i>Current Location</button>
+                <button onClick={this.props.checkDistance}><i class="lni lni-map-marker mr-20"></i>Current Location</button>
                 {/* <LocationSearchInput /> */}
             </div>
         ):(
