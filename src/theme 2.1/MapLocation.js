806e08a2e70cd28d5fe8b697472bd25d22fc27d2
vinayveerappaji <vinayveerappaji@gmail.com>
2020-09-17 11:14:04 +0530

    removed this for finctional components

--- src/theme 2.1/MapLocation.js
+++ src/theme 2.1/MapLocation.js
@@ -4,11 +4,11 @@ function MapLocation(props){
         return(
             <section id="location" class="pt-120 pb-10">
             <div className="container" >
-                <div className="google-map ">
+                <div className="google-map">
                     <iframe
                         frameborder="0" title="google-map" style={{border:"none",width:"100%",height:"100%"}}
                         src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDORUh0mGaVxDgP2ZojKCqVmpXnVOZfAS8
-                            &q=${this.props.restaurantName}+${this.props.address}`} allowfullscreen>
+                            &q=${props.restaurantName}+${props.address}`} allowfullscreen>
                     </iframe>
                 </div>
             </div>
