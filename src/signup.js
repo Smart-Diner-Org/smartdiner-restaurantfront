83d0cf954489b6fe62f1931f52a93199fc2ca3d4
vinayveerappaji <vinayveerappaji@gmail.com>
2020-09-22 08:52:51 +0530

    order later button added

--- src/signup.js
+++ src/signup.js
@@ -303,15 +303,16 @@ class SignUp extends Component{
             <div className="signup ">
                 <div className="container">
                     <div className="header row mt-30">
-                        <div className="col-lg-10 col-sm-12">
+                        <div className="col-lg-9 col-sm-12">
                             <Link to="/">
                                 <label className="mb-20"><i class="lni lni-arrow-left"></i>  Back to A3 Biryani </label>
                             </Link>
                             <h2>Customer Details</h2>
 
                         </div>
-                        <div className="col-lg-2 col-sm-12 d-flex align-items-end">
-                            <h4>Rs. {Number(sessionStorage.getItem("totalWithoutTax")).toFixed(2)}</h4>
+                        <div className="col-lg-3 col-sm-12 d-flex flex-column justify-content-end">
+                            {sessionStorage.getItem("deliveryDate") && <h6 className="row d-flex justify-content-end">Delivery Date: {sessionStorage.getItem("deliveryDate")}</h6> }
+                            <h4 className="row d-flex justify-content-end">Rs. {Number(sessionStorage.getItem("totalWithoutTax")).toFixed(2)}</h4>
                         </div>
                     </div>
                   
--- src/theme 2.1/Delivery.js
+++ src/theme 2.1/Delivery.js
@@ -4,21 +4,52 @@ import {Link} from 'react-router-dom'
 
 
 class Delivery extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            pre_order : false,
+            date : null,
+        }
+        this.handleChange = this.handleChange.bind(this)
+
+    }
+
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value,
+        })
+        sessionStorage.setItem("deliveryDate",value)
+    }
+
+
     
     render(){
         return(
             <div>
                 <hr/>
                 <div className="delivery-type">
+                { !this.state.pre_order &&
+                <>
                 <Link 
                     to={{
                         pathname: '/signup',
-                        totalPrice: 
-                            this.props.values[2].toFixed(2)
                         }} >
-                    <button >Checkout</button>
+                    <button >Order for Now</button>
                 </Link>
-                
+                <button onClick={()=>{this.setState({pre_order:true})}} className="mt-10">Order for Later</button>
+                </>
+                }
+
+                { this.state.pre_order &&
+                <>
+                <p>Select delivery date</p>
+                <input className="mt-10" name="date" required="true" type='date' onChange={this.handleChange} />
+                <Link to={this.state.date?"/signup":""}>
+                    <button className="mt-10" >Confirm</button>
+                </Link>
+                </>
+                }
                 
             </div>
             <hr/>
