17e35f8b54fc37375523bc2aa4068597071d46cd
vinayveerappaji <vinayveerappaji@gmail.com>
2020-09-26 16:46:52 +0530

    dropdown menu created

--- src/theme 2.1/Menu.js
+++ src/theme 2.1/Menu.js
@@ -1,48 +1,52 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 
-class Menu extends Component{
-    constructor(props){
-        super(props);
+class Menu extends Component {
+  constructor(props) {
+    super(props);
 
-        this.ele = []
+    this.ele = [];
+    this.onDropdownSelected = this.onDropdownSelected.bind(this)
+  }
 
-    }
-    
-      componentDidMount(){
-          //TODO : find which category has minimum 1 item and assing that value to cataegoryToBeShown variable
-        
-            this.ele[0].click();
-      }
-      
-    render(){
-        return(
-                <div class="col-lg-3 col-md-4">
-                    <div class="collection-menu text-center mt-20">
-                        
-                        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist"
-                            aria-orientation="vertical">
-                                {
-                                    this.props.categoryArray.map((category,index)=>{
-                                     
-                                        return(
+  componentDidMount() {
+    //TODO : find which category has minimum 1 item and assing that value to cataegoryToBeShown variable
+    this.props.setType(this.ele[0].value)
+  }
 
-                                           
+  onDropdownSelected(event){
+      this.props.setType(event.target.value);
+  }
 
-<a ref={a => this.ele[index] = a} className={index===0?"active":""}  id={`${category.name}`} data-toggle="pill"  onClick={()=>{this.props.setType(category.id)}}
-                                aria-selected="true" >{category.name}</a>
-                                          
-                                           
-                                            
-                                        )
-                                    })
-                                }                           
-                        </div> 
-                    </div> 
-                </div>
 
-        );
-    };
-
-   
-};
+  render() {
+    return (
+      <div class="col-lg-3 col-md-4">
+        <div class="collection-menu text-center mt-20">
+          <div
+            class="nav flex-column nav-pills"
+            id="v-pills-tab"
+            role="tablist"
+            aria-orientation="vertical"
+          >
+            <select className="menu-dropdown " onChange={(e)=>this.onDropdownSelected(e)} >
+              {this.props.categoryArray.map((category, index) => {
+                return (
+                  <option
+                    
+                    ref={(a) => (this.ele[index] = a)}
+                    className={index === 0 ? "active" : ""}
+                    value={`${category.id}`}
+                    data-toggle="pill"
+                  >
+                    {category.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default Menu;
