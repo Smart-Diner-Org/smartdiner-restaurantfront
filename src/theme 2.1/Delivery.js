0a3c014deb7188f4e40e9073290eba764f1122e7
vinayveerappaji <vinayveerappaji@gmail.com>
2020-09-30 12:56:27 +0530

    removed date if it not acceptable

--- src/theme 2.1/Delivery.js
+++ src/theme 2.1/Delivery.js
@@ -42,6 +42,7 @@ class Delivery extends Component {
       const timeLimit = 7200000;
       if (timeDifference <= timeLimit) {
         alert("You can pre-book the orders for at least 2 hours from now.");
+        document.getElementById("datepicker").value = null;
         return false;
       }
       sessionStorage.setItem("deliveryTime", time);
@@ -81,7 +82,7 @@ class Delivery extends Component {
               <>
                 <p className="mt-20">Choose your date and time</p>
                 <div className="delivery-type-inputs mt-10">
-                  <Flatpickr
+                  <Flatpickr id="datepicker"
                     options={
                       this.props.restaurant_website_detail
                         .is_pre_booking_time_required
