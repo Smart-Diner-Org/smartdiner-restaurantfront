fe91a9adbfd7cf7fdd8a4c26588df7f5a3cfa8fb
vinayveerappaji <vinayveerappaji@gmail.com>
2021-04-03 18:23:46 +0530

    made tp our orders

--- src/theme 2.1/Description.js
+++ src/theme 2.1/Description.js
@@ -41,7 +41,7 @@ class Description extends React.Component {
           {this.props.has_customisation_info && (
             <div className="call-for-order mt-70">
               <div class="section-title">
-                <h5 class="mb-15">Our Customized Orders</h5>
+                <h5 class="mb-15">Our Orders</h5>
               </div>
               <div className="row">
                 <div
